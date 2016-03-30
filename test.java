932006311ee76d813ce41c749abecabaf286a4f6
shiphitchcock1 <shiphitchcock1@gmail.com>
2016-03-30 13:09:54 +0530

    Update test.java

--- test.java
+++ test.java
@@ -8,13 +8,25 @@ class addtwonumbers_function
        {
               int sum= a+b;
        }
+       private static int subIntegers(int a, int b)
+       {
+              int sub= a-b;
+       }
  
        public static void main(String[] args)
        {
               // output the answer of adding both of the values together
               System.out.println("Answer = " + addIntegers(4, 4));
+              System.out.println("Answer2 = " + subIntegers(7, 4));
        }
        if(sum==8)
+       {
+          return true;
+        }
+        else {
+        return false
+        }
+        if(sub==3)
        {
           return true;
         }
