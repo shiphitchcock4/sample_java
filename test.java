cae031754a829fd0cb1fd7621b2df852c0400b14
shiphitchcock1 <shiphitchcock1@gmail.com>
2016-03-30 12:36:09 +0530

    Create test.java

---
+++ test.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+ 
+class addtwonumbers_function
+{
+       // private = local to this class
+       private static int addIntegers(int a, int b)
+       {
+              int sum= a+b;
+       }
+ 
+       public static void main(String[] args)
+       {
+              // output the answer of adding both of the values together
+              System.out.println("Answer = " + addIntegers(4, 4));
+       }
+       if(sum==8)
+       {
+          return true;
+        }
+        else {
+        return false
+        }
+}
